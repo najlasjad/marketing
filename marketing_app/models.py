b06e28fa9ed62456791e6ecb00b0dc17d06ad4b5
najlasjad <najla.asjad@gmail.com>
2025-07-28 00:09:29 +0700

    upload file

--- marketing_app/models.py
+++ marketing_app/models.py
@@ -2,25 +2,6 @@ import os
 from django.db import models
 
 
-# class RegistrationData(models.Model):
-#     idregistrantdata = models.IntegerField()
-#     groupreg = models.IntegerField()
-#     regtype = models.IntegerField()
-#     iddataregkhusustype = models.IntegerField()
-#     idschooltypedata = models.IntegerField()
-#     idschooljurusandata = models.IntegerField()
-#     email = models.EmailField()
-#     idmajordata = models.IntegerField()
-#     idcountrydata = models.IntegerField()
-#     iddataprovinces = models.IntegerField()
-#     iddataregencies = models.IntegerField()
-#     ispaid = models.BooleanField()  # Dulu BooleanField → sekarang IntegerField
-#     paymentamount = models.IntegerField()  # Karena CSV-nya integer, bukan decimal
-
-#     def __str__(self):
-#         return str(self.idregistrantdata)
-
-
 def upload_to_data_folder(instance, filename):
     # Simpan file di folder "document/" dengan nama asli user
     return os.path.join('documents', filename)
--- marketing_app/views.py
+++ marketing_app/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-# from django.contrib import messages
-# import pandas as pd
-
-# from .forms import UploadCSVForm
-# from .models import RegistrationData
 
 from .forms import DocumentForm
 
